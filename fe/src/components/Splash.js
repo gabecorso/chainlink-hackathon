9f297a1728b57338b849c2a9a3ea75335daa0964
Alex <schroedergoncalves@gmail.com>
2021-03-24 18:23:54 -0400

    created a background gradiant animation for the landing-loading page

--- fe/src/components/Splash.js
+++ fe/src/components/Splash.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import splash from '../assets/styles/sass/splash.sass';
+import splash from '../assets/styles/splash.sass';
 
 export default function Splash() {
     return (
         <div>
-            <h1>iOwnThis<h2> </h2></h1>
+            <h1>iOwnThis</h1>
         </div>
     )
 }
