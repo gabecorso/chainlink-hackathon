9ad493b92fc97c65d1afbc6c7de976d7d18567b7
Brian Hou <brianghou@gmail.com>
2021-04-03 18:38:10 -0700

    index.js heroku home page

--- backend/src/server.js
+++ backend/src/server.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === 'production') {
 
 
     app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname,'/../../fe/build/index.html'));
+        res.sendFile(path.join(__dirname,'/../../fe/build/index.js'));
     });
 }
 
