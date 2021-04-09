9f54721dea3502720fa005ee3aa617443a075887
Brian Hou <brianghou@gmail.com>
2021-04-09 09:47:38 -0700

    changed dir path

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,11 +30,11 @@ app.use('/api', routes.apiRoutes);
 
 //Test Heroku deployment
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join( __dirname, '/../../fe/build')));
+    app.use(express.static(path.join( __dirname, '../fe/build')));
 
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname,'/../../fe/build/index.html'));
+        res.sendFile(path.join(__dirname,'../fe/build/index.html'));
     });
 }
 
