8bccd7ce1250f07b07c15f2a0475c6cdacc25c0d
Alex <schroedergoncalves@gmail.com>
2021-04-29 17:08:27 -0400

    some additional styles (with breakpoints) for the create company form and tasks page

--- fe/src/components/company/CreateCompany.js
+++ fe/src/components/company/CreateCompany.js
@@ -10,7 +10,7 @@ const initCompanyForm = {
     companyName: '',
     companyBio: '',
     tokens: 500000,
-    tokenPrice: 2,
+    tokenPrice: '2.0',
     tldAddress: '',   
     // terms: false,     
 }
@@ -19,8 +19,8 @@ const validationSchema = yup.object({
     companyName: yup.string().required(),
     companyBio: yup.string().required(),
     tldAddress: yup.string().required(),
-    tokens: yup.string().required(),
-    tokenPrice: yup.number().required(),
+    tokens: yup.number().required(),
+    tokenPrice: yup.string().required(),
     // terms: yup.bool().required().oneOf([true], 'Terms must be accepted'),
 })
 
@@ -35,6 +35,9 @@ export default function CreateCompany() {
         style: 'currency',
         currency: 'USD',
     });
+    const numWithCommas = (int) => {
+        return int.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
     
     const postNewCompany = company => {
         console.log('servers? who needs em?');
@@ -43,7 +46,7 @@ export default function CreateCompany() {
     return (
         <Layout>
             <Container className="form-container mt-1">
-                <h3 className="create-company-title">Create a Company</h3>
+                <h2 className="create-company-title">Create a Company</h2>
                 <Formik 
                     validationSchema={validationSchema}
                     initialValues={initCompanyForm}
@@ -55,13 +58,13 @@ export default function CreateCompany() {
                             values, touched, errors, dirty, 
                             isSubmitting, handleChange, handleSubmit
                         }) => (
-                    <Form className="create-company-form" onSubmit={handleSubmit}>
+                    <Form className={"create-company-form mt-3"} onSubmit={handleSubmit}>
                         <Form.Group controlId="company-name">
                             <Form.Label>Company Name</Form.Label>
                             <Form.Control 
                                 type="text"
                                 name="companyName"
-                                placeholder="International Business Machines Corporation"
+                                placeholder="International Business Machines"
                                 onChange={handleChange}
                                 value={values.companyName}
                             />
@@ -79,21 +82,16 @@ export default function CreateCompany() {
                         </Form.Group>
                         <Form.Group controlId="company-ticker">
                             <Form.Label>Company Ticker</Form.Label>
-                            <InputGroup hasValidation>
-                                <InputGroup.Prepend>
-                                    <InputGroup.Text id="inputGroupPrepend">$</InputGroup.Text>
-                                </InputGroup.Prepend>
                             <Form.Control
                                 type="text"
                                 name="tldAddress" 
-                                placeholder="$IBM"
+                                placeholder="IBM"
                                 onChange={handleChange}
                                 value={values.tldAddress}
                             />
-                            <Form.Text className="w-100">This will be your company's public address</Form.Text>
-                            </InputGroup>
+                            <Form.Text className="w-100">This will be your company's public wallet address.</Form.Text>
                         </Form.Group>
-                        <Form.Group controlId="num-shares">
+                        <Form.Group controlId="num-shares" id="share-slider">
                                 <Form.Label>Shares to Distribute</Form.Label>
                                 <Form.Control 
                                     type="range"
@@ -107,23 +105,30 @@ export default function CreateCompany() {
                                     Choose a number of shares between 1,000 & 1,000,000.
                                 </Form.Text>
                         </Form.Group>
-                        <Card className="share-price-card">
-                            <Form.Group controlId="price-shares">
-                                <Form.Label>Price per Share:</Form.Label>
-                                <Form.Control 
-                                    type="number"
-                                    placeholder="$0.01 to $10,000.00"
-                                    className=""
-                                    name="tokenPrice"
-                                    value={(values.tokenPrice)}
-                                    onChange={handleChange}
+                        <h4 className={"m-3 num-shares"}>Number of Shares: <span id="share-num">{numWithCommas(values.tokens)}</span></h4>
+                        <Form.Group controlId="price-shares" >
+                            <Form.Label>Price per Share:</Form.Label>
+                            <InputGroup hasValidation>
+                                <InputGroup.Prepend>
+                                    <InputGroup.Text id="inputGroupPrepend">$</InputGroup.Text>
+                                </InputGroup.Prepend>
+                            <Form.Control 
+                                type="number"
+                                placeholder="$0.01 to $10,000.00"
+                                className=""
+                                name="tokenPrice"
+                                value={(values.tokenPrice)}
+                                onChange={handleChange}
                                 />
-                            </Form.Group>
-                            <div>
-                            Total Valuation: {currencyFormatter.format(values.tokens * values.tokenPrice)}
-                            </div>
+                            </InputGroup>
+                        </Form.Group>
+                        
+                        <Card className="share-price-card">
+                            <h3 className={"d-flex flex-column align-items-center"}>
+                            Total Valuation: <span>{currencyFormatter.format(values.tokens * values.tokenPrice)}</span>
+                            </h3>
                         </Card>
-                        <Button onClick={handleShow}>
+                        <Button className={"mt-4 cont-btn"}onClick={handleShow}>
                             Continue
                         </Button>
                         <Modal className={"company-summary"} style={{display: "grid"}} show={modalIsOpen} onHide={handleClose}>
@@ -136,7 +141,7 @@ export default function CreateCompany() {
                                     <li id="summ-name">Company Name: <strong>{values.companyName}</strong></li>
                                     <li id="summ-descr">Company Description: <strong>{values.companyBio}</strong></li>
                                     <li id="summ-ticker">Company Ticker: <strong>{values.tldAddress}</strong></li>
-                                    <li id="summ-shares">Number of Shares: <strong>{values.tokens}</strong></li>
+                                    <li id="summ-shares">Number of Shares: <strong>{numWithCommas(values.tokens)}</strong></li>
                                     <li id="summ-price">Price per Share: <strong>{currencyFormatter.format(values.tokenPrice)}</strong></li>
                                     <li id="summ-val">Proposed Valuation: <strong>{currencyFormatter.format(values.tokenPrice * values.tokens)}</strong></li>
                                 </ul>
