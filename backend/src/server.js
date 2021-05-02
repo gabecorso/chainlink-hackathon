f5d663d8cfc70b439e07a646c1688a598ba77ae2
Alex <schroedergoncalves@gmail.com>
2021-05-02 16:45:30 -0400

    renamed 'fe' folder 'client' and chaged all references to it for clarity

--- backend/src/server.js
+++ backend/src/server.js
@@ -31,11 +31,11 @@ app.use('/api', routes.apiRoutes);
 
 //Test Heroku deployment
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join( __dirname, '../../fe/build')));
+    app.use(express.static(path.join( __dirname, '../../client/build')));
     
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname,'../../fe/build/index.html'));
+        res.sendFile(path.join(__dirname,'../../client/build/index.html'));
     });
 }
 
